255a5f12a6cd9bc3a811f2a4fdac08644f5cc9cb
Mohammed Amine Regragui <aminereg94@gmail.com>
2025-09-30 15:16:48 +0000

    dbt_testing21

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -1,6 +1,6 @@
-select  campaign_name,key,date_date,sum(ads_cost)as ads_cost
+select  campaign_name,date_date,sum(ads_cost)as ads_cost
 ,sum(impression)as imression,
 sum(click) as click
  from {{ ref('int_campaigns') }} 
-group by campaign_key,date_date
+group by campaign_name,date_date
 order by date_date desc
